39fd7ed71f967b9288fe1bb0f6e69f3944201c4f
Vitor Duggen <zduggem@gmail.com>
2022-08-22 17:44:06 -0300

    Importado globalmente o main.scss

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/scss/main.scss";
+        `,
+      },
+    },
+  },
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@atoms': path.resolve(__dirname, 'src/components/atoms/index'),
+        '@organisms': path.resolve(__dirname, 'src/components/organisms/index'),
+        '@molecules': path.resolve(__dirname, 'src/components/molecules/index'),
+        '@bosons': path.resolve(__dirname, 'src/components/bosons/index'),
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
 });
